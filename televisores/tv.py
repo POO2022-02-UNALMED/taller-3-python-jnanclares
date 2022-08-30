a2bc5edce3d8bf219ab318c0bf067025eaf2308a
jnanclares <73925961+jnanclares@users.noreply.github.com>
2022-08-30 00:17:35 -0500

    intento 1

---
+++ televisores/tv.py
@@ -0,0 +1,79 @@
+
+
+class TV:
+    numTV = 0
+    def __init__(self, marca, estado):        
+        self._marca = marca
+        self._canal = 1
+        self._precio = 500
+        self._estado = estado
+        self._volumen = 1
+        self._control = None
+        TV.numTV += 1
+    
+    # Get y set
+    def getMarca(self):
+        return self._marca
+    
+    def setMarca(self, marca):
+        self._marca = marca
+    
+    def getControl(self):
+        return self._control
+    
+    def setControl(self, control):
+        self._control = control
+    
+    def getPrecio(self):
+        return self._precio
+    
+    def setPrecio(self, precio):
+        self._precio = precio
+    
+    def getVolumen(self):
+        return self._volumen
+    
+    def setVolumen(self, volumen):
+        self._volumen = volumen
+    
+    def getCanal(self):
+        return self._canal
+    
+    def setCanal(self, canal):
+        if self._canal > 1 and self._canal < 120 and self._estado:
+            self._canal = canal
+    
+    @classmethod
+    def setNumTV(cls, numTV):
+        cls.numTV = numTV
+    
+    def getNumTV():
+        return TV.numTV
+        
+    # Modificar estado
+    def turnOn(self):
+        self._estado = True
+    
+    def turnOff(self):
+        self._estado = False
+        
+    def getEstado(self):
+        return self._estado
+    
+    # Modificar canal
+    def canalUp(self):
+        if self._canal >= 1 and self._canal < 120 and self._estado:
+            self._canal += 1
+    def canalDown(self):
+        if self._canal > 1 and self._canal <= 120 and self._estado:
+            self._canal -= 1
+    
+    # Modificar volumen
+    def volumenUp(self):
+        if self._volumen >= 0 and self._volumen < 7 and self._estado:
+            self._volumen += 1
+    def volumenDown(self):
+        if self._volumen > 0 and self._volumen <= 7 and self._estado:
+            self._volumen -= 1
+    
+    
